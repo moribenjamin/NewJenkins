26d2f37d380defc232995ec7c1f08af18accb4e5
Mori Benjamin <30694453+moribenjamin@users.noreply.github.com>
2022-12-13 17:11:38 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args){
-		System.out.println("---**** Hello World & Welcome to Mori's Jenkins Training**** Made a Change again
-				   -");
+		System.out.println("---**** Hello World & Welcome to Mori's Jenkins Training**** Made a Change-");
 		
 	}
 }
